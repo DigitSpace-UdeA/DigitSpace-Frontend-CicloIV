bc51e62b54e91b4bc4283f4f3a2e4126d21a964e
katerin <katerinbarrera2001@gmail.com>
2021-12-15 20:41:50 -0500

    push

---
+++ digit-space/src/graphql/proyectos/mutations.js
@@ -0,0 +1,24 @@
+import { gql } from "@apollo/client";
+
+const EDITAR_PROYECTOLIDER = gql`
+  mutation EditarProyecto($_id: String!, $campos: camposProyecto!) {
+    editarProyecto(_id: $_id, campos: $campos) {
+      _id
+      nombre
+      presupuesto
+    }
+  }
+`;
+
+const EDITAR_PROYECTOADMINISTRADOR = gql`
+  mutation Mutation($_id: String!, $campos: camposProyecto!) {
+    editarProyecto(_id: $_id, campos: $campos) {
+      _id
+      nombre
+      estadoProyecto
+      faseProyecto
+    }
+  }
+`;
+
+export { EDITAR_PROYECTOLIDER, EDITAR_PROYECTOADMINISTRADOR };
