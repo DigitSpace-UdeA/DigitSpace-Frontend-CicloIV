52c51f06dd7d769c949abf09972203aa2eef7dcb
Yesica <yesicavillamil@gmail.com>
2021-12-18 02:50:38 -0500

    cambiar rutas app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -84,22 +84,19 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<PrivateLayout />}>
-                <Route path="" element={<Index />} />
+                <Route path="inicio" element={<Index />} />
                 <Route path="usuarios" element={<IndexUsuarios />} />
                 <Route
                   path="usuarios/editar/:_id"
-                  element={<EditarUsuario />}
-                />
+                  element={<EditarUsuario />}/>
                 <Route path="proyectos" element={<IndexProyectos />} />
                 <Route path="proyectos/nuevo" element={<NuevoProyecto />} />
                 <Route
                   path="proyectos/listarAvances/:_id"
-                  element={<ListarAvance />}
-                />
+                  element={<ListarAvance />}/>
                 <Route
                   path="proyectos/registrarAvances/:_id"
-                  element={<RegistrarAvance />}
-                />
+                  element={<RegistrarAvance />}/>
                 <Route path="inscripciones" element={<IndexInscripciones />} />
               </Route>
               <Route path="/auth" element={<AuthLayout />}>
